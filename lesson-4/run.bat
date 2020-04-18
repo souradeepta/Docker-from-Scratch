ce0d3ddb7c68638f0b905751d76936c955b578bc
Souradeepta Biswas <sdb.svnit@gmail.com>
2020-04-18 18:23:39 -0500

    fixed with port binding

--- lesson-4/run.bat
+++ lesson-4/run.bat
@@ -2,4 +2,4 @@ ECHO OFF
 
 ECHO nodejs image
 
-docker run -it --rm --name node -d -v "%CD%" -w /src node:7.7.4-alpine node app.js
+docker run -it --rm --name node -d -v "%CD%" -w /src -p 8081:3000 node:7.7.4-alpine node app.js
