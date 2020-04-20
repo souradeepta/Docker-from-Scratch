9c7a15d85d9c2b9b4a1e4d52104dd4971183930e
Souradeepta Biswas <sdb.svnit@gmail.com>
2020-04-19 22:49:32 -0500

    running go container

---
+++ lesson-8/app.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I am a Go app running from a scratch Docker container")
+
+}
--- lesson-8/run.bat
+++ lesson-8/run.bat
@@ -1,3 +1,15 @@
 ECHO off
 
-ECHO use VS 2017 to run the demo
+ECHO Building app
+
+docker run --rm -v "%CD%":/ a-w /a  golang:1.14.2 go build -v -o app
+
+ECHO Creating image
+
+docker build -t dfs-scratch -f Dockerfile.run .
+
+ECHO Running container
+
+docker run -rm dfs-scratch
+
+docker rmi dfs-scratch
